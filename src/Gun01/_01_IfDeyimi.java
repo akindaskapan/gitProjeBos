77a803f8959fdeba37ef6a60dccebd39d172e487
akindaskapan <akin.daskapan@hotmail.com>
2023-09-04 19:38:21 +0300

    akın baba burada yazıldı

--- src/Gun01/_01_IfDeyimi.java
+++ src/Gun01/_01_IfDeyimi.java
@@ -4,6 +4,9 @@ public class _01_IfDeyimi {
     public static void main(String[] args) {
         System.out.println("Merhaba Dünya");
 
+        System.out.println("akın baba burada");
+
+
 
     }
 }
